600cc9608ffe54129b65b9564e41040fbde96f24
Danil Pismenny <danil@orionet.ru>
2012-07-09 14:08:41 +0400

    key or url for gdoc
    strip_values добавлен

--- lib/ext/google_drive.rb
+++ lib/ext/google_drive.rb
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 #
+require 'ostruct'
+
 module GoogleDrive
   mattr_accessor :email, :password
 
-  def self.open key
+  def self.open_spreadsheet key
     @last_session = login email, password
-    @last_session.spreadsheet_by_key key
+
+    if key=~/google.com/
+      @last_session.spreadsheet_by_url key
+    else
+      @last_session.spreadsheet_by_key key
+    end
   end
 
   class SuperRow
@@ -33,12 +40,10 @@ module GoogleDrive
           class_eval do
             define_method key do
               get key
-              # config.ws[row_num, config.params_h[key]]
             end
 
             define_method "#{key}=" do |value|
               set key, value
-              # config.ws[row_num, config.params_h[key]] = value
             end
           end
         end
@@ -52,7 +57,9 @@ module GoogleDrive
 
       raise "No such column name '#{key}'" unless params_h.has_key? key
 
-      ws[ row_num, params_h[key] ]
+      value = ws[ row_num, params_h[key] ]
+
+      config.opts.strip_values ? value.strip : value
     end
 
     def set key, value
@@ -67,21 +74,24 @@ module GoogleDrive
   end
 
   class SuperConfig
-    attr_accessor :params_a, :params_h, :ws, :row_class
+    attr_accessor :params_a, :params_h, :ws, :opts
+
+    delegate :row_class, :first_content_row, :to => :opts
 
     delegate :save, :num_rows, :num_cols, :to => :ws
 
-    def initialize ws, _row_class = SuperRow
+    def initialize _ws, _opts={}
       self.params_a = []
       self.params_h = {}
-      self.row_class = _row_class
-      self.ws = ws
+      self.ws = _ws
 
-      ws.rows[0].each_with_index do |v,i|
-        params_a[i] = v
-        next if v.blank?
-        params_h[v] = i+1
-      end
+      self.opts = OpenStruct.new _opts.reverse_merge(
+        :row_class => SuperRow,
+        :first_content_row => 2,
+        :strip_values => true
+      )
+
+      init_params
 
       after_init
     end
@@ -91,6 +101,22 @@ module GoogleDrive
 
     def super_row row_num
       row_class.new self, row_num
+    end
+
+    def each_content_row &block
+      for row in first_content_row..num_rows
+        block.call super_row(row)
+      end
+    end
+
+    protected
+
+    def init_params
+      ws.rows[0].each_with_index do |v,i|
+        params_a[i] = v
+        next if v.blank?
+        params_h[v] = i+1
+      end
     end
   end
 
